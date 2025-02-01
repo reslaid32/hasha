4b15f861107de786185226b402deaf0016d01db9
Hayes <zangetsuebaw@gmail.com>
2025-02-01 17:39:34 +0200

    Update ver.c

--- src/ver.c
+++ src/ver.c
@@ -6,6 +6,6 @@ HASHA_PUBLIC_FUNC hashaver_t hashaver(void) {
     hashaver_t hashav;
     hashav.major = 1;
     hashav.minor = 0;
-    hashav.patch = 3;
+    hashav.patch = 4;
     return hashav;
 }
