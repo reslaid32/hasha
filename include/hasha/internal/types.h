e1e40a5c6d3b571507c302d3cc985bf9e52efc79
reslaid32 <goteevreslajd@gmail.com>
2025-03-25 15:22:38 +0200

    Update types.h

--- include/hasha/internal/types.h
+++ include/hasha/internal/types.h
@@ -9,8 +9,10 @@
 #define __ha_buf __ha_byte*
 #define __ha_in_buf_type const __ha_buf
 #define __ha_out_buf_type __ha_buf
+typedef __ha_buf ha_buf_t;
 typedef __ha_in_buf_type ha_inbuf_t;
 typedef __ha_out_buf_type ha_outbuf_t;
+typedef ha_outbuf_t ha_digest_t;
 #define ha_types_defined
 #endif /* ha_types_defined */
 
